b1d0f726286788564e55d68cbc05b308e9a6bcb6
Asaf Ben Natan <asaf@wizzdi.com>
2022-07-29 02:07:04 +0300

    fixed getFileResource

--- source/api/upload.service.ts
+++ source/api/upload.service.ts
@@ -105,22 +105,36 @@ export class UploadService {
                 }
             });
     }
-
-    /**
+ /**
      * 
      * @param md5 
      * @param authenticationkey The AuthenticationKey retrieved when sign-in into the system
      */
-    public getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any): Observable<FileResource> {
-        return this.getFileResourceWithHttpInfo(md5, authenticationkey, extraHttpRequestParams)
-            .map((response) => {
-                if (response.status === 204) {
-                    return undefined;
-                } else {
-                    return FlexiCoreDecycle.retrocycle(response.body) || {};
-                }
-            });
-    }
+  public getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any, observe?: 'body', reportProgress?: boolean): Observable<FileResource>;
+  public getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<FileResource>>;
+  public getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<FileResource>>;
+  public getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any, observe: any = 'body', reportProgress: boolean = false): Observable<any> {
+     const path = this.basePath + '/resources/${md5}'
+         .replace('${' + 'md5' + '}', String(md5));
+
+     let headers = this.defaultHeaders;
+
+     if (md5 === null || md5 === undefined) {
+         throw new Error('Required parameter md5 was null or undefined when calling getFileResource.');
+     }
+     if (authenticationkey !== undefined && authenticationkey !== null) {
+         headers = headers.set('authenticationkey', String(authenticationkey));
+     }
+
+     return this.httpClient.get<FileResource>(path,
+         {
+             withCredentials: this.configuration.withCredentials,
+             headers: headers,
+             observe: observe,
+             reportProgress: reportProgress
+         }
+     ).pipe(map(o=>FlexiCoreDecycle.retrocycle(o)));
+ }
 
     /**
      * 
@@ -374,49 +388,6 @@ export class UploadService {
         return this.httpClient.request(requestOptions).map(o => FlexiCoreDecycle.retrocycle(o));
     }
 
-    /**
-     * 
-     * 
-     * @param md5 
-     * @param authenticationkey The AuthenticationKey retrieved when sign-in into the system
-     */
-    public getFileResourceWithHttpInfo(md5: string, authenticationkey?: string, extraHttpRequestParams?: any): Observable<any> {
-        const path = this.basePath + '/resources/${md5}'
-            .replace('${' + 'md5' + '}', String(md5));
-
-        let queryParameters = new URLSearchParams();
-        let headers = this.defaultHeaders; // https://github.com/angular/angular/issues/6845
-
-        // verify required parameter 'md5' is not null or undefined
-        if (md5 === null || md5 === undefined) {
-            throw new Error('Required parameter md5 was null or undefined when calling getFileResource.');
-        }
-        if (authenticationkey !== undefined && authenticationkey !== null) {
-            headers = headers.set('authenticationkey', String(authenticationkey));
-        }
-
-
-        // to determine the Accept header
-        let produces: string[] = [
-            'application/json'
-        ];
-
-
-        let requestOptions = new HttpRequest(
-            'GET',
-            path,
-            {
-                headers: headers,
-                search: queryParameters,
-                withCredentials: this.configuration.withCredentials
-            });
-        // https://github.com/swagger-api/swagger-codegen/issues/4037
-        if (extraHttpRequestParams) {
-            requestOptions = (<any>Object).assign(requestOptions, extraHttpRequestParams);
-        }
-
-        return this.httpClient.request(requestOptions);
-    }
 
     /**
      * 
