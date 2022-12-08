fe8aa55cc49f9e193a6305a8b09ec57767fccf5d
Zukic Mirzet <zukicmirzet94@gmail.com>
2022-12-08 14:17:11 +0100

    Remove lastExecuted

--- source/model/exportDynamicInvokerToCSVRequest.ts
+++ source/model/exportDynamicInvokerToCSVRequest.ts
@@ -1,7 +1,6 @@
 import { ExecuteInvokerRequest } from "./executeInvokerRequest";
 
 export interface ExportDynamicInvokerToCSVRequest extends ExecuteInvokerRequest { 
-    lastExecuted?: Date;
     csvFormat?: any;
     fieldProperties?: any;
 }
