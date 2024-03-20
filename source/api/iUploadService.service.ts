c06ab79deb66e0bcfc8228694145f3c0e51fa00a
Mirzet Zukic <zukicmirzet94@gmail.com>
2024-03-20 14:28:13 +0100

    Fix fc migration's bugs

--- source/api/iUploadService.service.ts
+++ source/api/iUploadService.service.ts
@@ -2,7 +2,7 @@ import { Observable } from "rxjs";
 import { FileResource } from "..";
 
 export interface IUploadService{
-    getFileResource(md5: string, authenticationkey?: string, extraHttpRequestParams?: any): Observable<FileResource>;
+    getFileResource(md5: string, extraHttpRequestParams?: any): Observable<FileResource>;
     uploadFileWithChunkMd5(md5?: string, name?: string, chunkMd5?: string, lastChunk?: boolean, blob?: Blob, extraHttpRequestParams?: any): Observable<FileResource>;
 
 }
